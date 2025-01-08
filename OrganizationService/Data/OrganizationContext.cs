5a0a6de7a8c4a1e54a827674dd8ba65b302e54c5
N0Fearz <55428530+N0Fearz@users.noreply.github.com>
2025-01-08 23:03:32 +0100

    removed comment

--- OrganizationService/Data/OrganizationContext.cs
+++ OrganizationService/Data/OrganizationContext.cs
@@ -6,6 +6,5 @@ namespace OrganizationService.Data
     public class OrganizationContext(DbContextOptions<OrganizationContext> options) : DbContext(options)
     {
         public DbSet<Organization> Organizations { get; set; }
-        //public DbSet<User> Users { get; set; }
     }
 }
