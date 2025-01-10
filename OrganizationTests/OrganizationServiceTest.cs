bfd30f2c7efc81a8e09c71edc44d1e9d83c12264
N0Fearz <55428530+N0Fearz@users.noreply.github.com>
2025-01-10 11:13:38 +0100

    uncommented field

--- OrganizationTests/OrganizationServiceTest.cs
+++ OrganizationTests/OrganizationServiceTest.cs
@@ -19,17 +19,17 @@ public class OrganizationServiceTest
         _organizationRepository = Substitute.For<IOrganizationRepository>();
         _organizationService = new OrganizationService.Services.OrganizationService(_organizationRepository, migrationService);
     }
-    // [Fact]
-    // public void AddOrganization_Should_Return_SchemaName()
-    // {
-    //     //arrange
-    //     var organizationMessage =
-    //         "{\"id\":\"ac4ec53c-45da-4bec-939a-08e9a360f546\",\"name\":\"organization\",\"alias\":\"organization\",\"enabled\":true,\"description\":\"\",\"redirectUrl\":\"\",\"attributes\":{},\"domains\":[{\"name\":\"1233.bsr\",\"verified\":false}]}";
-    //     //act
-    //     var result = _organizationService.AddOrganization(organizationMessage);
-    //     //assert
-    //     Assert.Equal("schema_organization", result);
-    // }
+    [Fact]
+    public void AddOrganization_Should_Return_SchemaName()
+    {
+        //arrange
+        var organizationMessage =
+            "{\"id\":\"ac4ec53c-45da-4bec-939a-08e9a360f546\",\"name\":\"organization\",\"alias\":\"organization\",\"enabled\":true,\"description\":\"\",\"redirectUrl\":\"\",\"attributes\":{},\"domains\":[{\"name\":\"1233.bsr\",\"verified\":false}]}";
+        //act
+        var result = _organizationService.AddOrganization(organizationMessage);
+        //assert
+        Assert.Equal("schema_organization", result);
+    }
     
     [Fact]
     public void AddOrganization_Should_Return_error()
